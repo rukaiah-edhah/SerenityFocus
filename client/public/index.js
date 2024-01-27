e3377e765303714bc9c8d0d09bfbfbf9549ae509
Shevan Abdulla <shevan.jabary@gmail.com>
2024-01-26 19:56:45 -0500

    Worked on timer and fixed pugs with CSS for sign in

--- client/public/index.js
+++ client/public/index.js
@@ -1,4 +1,4 @@
-let popup = document.getElementById("popup")
+let popup = document.getElementById("about")
 
 function openPopup(){
 	closeSignin();
@@ -19,23 +19,3 @@ function openSignin(){
 function closeSignin(){
 	signIn.classList.remove("open-sign_in")
 }
-
-/*
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-*/

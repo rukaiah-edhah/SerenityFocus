290e730df14be29131bdcfc7dace8cda39c90f05
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2024-01-29 12:50:23 -0500

    Added sign-up form

--- client/public/index.js
+++ client/public/index.js
@@ -2,24 +2,46 @@
 
 let popup = document.getElementById("about")
 
-function openPopup(){
-	closeSignin();
-	popup.classList.add("open-popup");
+function openPopup() {
+    closeSignUp();
+    closeSignIn();
+    popup.classList.add("open-popup");
 }
 
-function closePopup(){
-	popup.classList.remove("open-popup");
+function closePopup() {
+    popup.classList.remove("open-popup");
 }
 
-let signIn = document.getElementById("sign_in")
+let signUp = document.getElementById("sign_up");
+let signIn = document.getElementById("sign_in");
 
-function openSignin(){
-	closePopup();
-	signIn.classList.add("open-sign_in")
+function openSignUp() {
+    closePopup();
+    closeSignIn();  
+    signUp.classList.add("open-sign_up");
 }
 
-function closeSignin(){
-	signIn.classList.remove("open-sign_in")
+function closeSignUp() {
+    signUp.classList.remove("open-sign_up");
+}
+
+function openSignIn() {
+    closePopup();
+    closeSignUp();  
+    signIn.classList.add("open-sign_in");  
+}
+
+function closeSignIn() {
+    signIn.classList.remove("open-sign_in");  
+}
+
+function goBack() {
+    closeSignUp();  
+    openSignIn();  
+}
+
+function openForgetPassword() {
+    // You can open another pop-up or redirect to a forget password page
 }
 
 // Timer

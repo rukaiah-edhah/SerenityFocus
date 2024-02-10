bd555eaa61924feb3ce32df852024cae2e48784c
Shevan Abdulla <shevan.jabary@gmail.com>
2024-02-10 13:15:46 -0500

    fixed an issue with the sign-up button not linked to the signup.html path and created sigh_up.js file to see if that will fix the event listiner issue that made the app to crash when Enter is pressed

--- client/public/index.js
+++ client/public/index.js
@@ -165,11 +165,13 @@ document.addEventListener('DOMContentLoaded', function () {
 			const checkbox = document.createElement('input');
 			checkbox.type = 'checkbox';
 			checkbox.name = 'checkbox';
+			checkbox.id = 'test1'
 			checkbox.className = 'checkbox-field';
 			
 			const label = document.createElement('label');
 			label.className = 'checkbox-label';
 			label.textContent = taskText;
+			label.for = 'test1'
 			
 			// Append checkbox and label to the task element
 			taskElement.appendChild(checkbox);
@@ -192,36 +194,3 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	});
 });
-
-// sign up form submission 
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('signup-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        const formData = {
-            firstName: document.getElementById('signup_first_name').value,
-            lastName: document.getElementById('signup_last_name').value,
-            username: document.getElementById('signup_username').value,
-            email: document.getElementById('signup_email').value,
-            password: document.getElementById('signup_password').value,
-        };
-
-        fetch('/api/signup', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(formData),
-        })
-        .then(response => {
-            if(response.ok) {
-                return response.json();
-            }
-            throw new Error('Signup failed');
-        })
-        .then(data => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-    });
-}); 

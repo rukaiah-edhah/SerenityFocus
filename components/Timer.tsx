b1379386ceb9ba1416105bee2f310e280886498c
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-03-07 19:04:20 -0500

    timer works

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -4,24 +4,57 @@ import { Button } from "@/components/ui/button"
 
 
 export default function Timer(){
-    const initialTime = 24 * 60 + 59;
-    const [timeLeft, setTimeLeft] = useState(initialTime);
+    let countdownInterval: any;
+    let paused = false;
+    const [timeLeft, setTimeLeft] = useState(0);
+    const [totalSeconds, setTotalSeconds] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+
+    const updateTime = (increment: number) => {
+        if (!paused && !countdownInterval && totalSeconds < 3600){
+            setTotalSeconds((prevSeconds) => prevSeconds + increment);
+        }
+    }
+
+    const formatTime = (time: number) => ({
+        '--value': time,
+    }) as React.CSSProperties;
+
+    const addFive = () => setTimeLeft((prevTime) => {
+        if (prevTime < 3600){
+            return prevTime + 300;
+        }
+        return prevTime;
+    });
+    const subFive = () => setTimeLeft((prevTime) => Math.max(0, prevTime - 300));
+
+    const startCountdown = () => setIsRunning(true);
+    const pauseCountdown = () => setIsRunning(false);
+
+    const resetCountdown = () => {
+        clearInterval(countdownInterval);
+        setTotalSeconds(0);
+        paused = false;
+        setTimeLeft(0);
+    }
 
     useEffect(() => {
-        if (!timeLeft) return;
+        if (!isRunning || timeLeft <= 0) return;
 
         const intervalId = setInterval(() => {
             setTimeLeft(timeLeft - 1);
         }, 1000);
 
         return () => clearInterval(intervalId);
-    }, [timeLeft])
+    }, [isRunning, timeLeft])
 
     const minutes = Math.floor(timeLeft / 60);
     const seconds = timeLeft % 60;
 
-    const formattedMinutes = { '--value': minutes} as React.CSSProperties;
-    const formattedSeconds = { '--value': seconds} as React.CSSProperties;
+    const formattedMinutes = formatTime(minutes);
+    const formattedSeconds = formatTime(seconds);
+
+
 
     return(
         <div>
@@ -44,21 +77,21 @@ export default function Timer(){
             <div className='flex flex-col items-center justify-center'>
                 <div className='items-center justify-center grid grid-cols-2 gap-5 lg:gap-6 w-full mb-5'>
                     <div>
-                        <Button type='button' className='w-full bg-base-content text-base-300'>+5</Button>
+                        <Button type='button' onClick={() => addFive()} className='w-full bg-base-content text-base-300'>+5</Button>
                     </div>
                     <div>
-                        <Button type='button' className='w-full bg-base-content text-base-300'>-5</Button>
+                        <Button type='button' onClick={() => subFive()} className='w-full bg-base-content text-base-300'>-5</Button>
                     </div>
                 </div>
                 <div className='items-center justify-center grid grid-cols-3 gap-4 lg:gap-6 w-full'>
                     <div>
-                        <Button type='button' className='w-full bg-base-content text-base-300'>Start</Button>
+                        <Button type='button' onClick={() => startCountdown()} className='w-full bg-base-content text-base-300'>Start</Button>
                     </div>
                     <div>
-                        <Button type='button' className='w-full bg-base-content text-base-300'>Pause</Button>
+                        <Button type='button' onClick={() => pauseCountdown()} className='w-full bg-base-content text-base-300'>Pause</Button>
                     </div>
                     <div>
-                        <Button type='button' className='w-full bg-base-content text-base-300'>Reset</Button>
+                        <Button type='button' onClick={() => resetCountdown()} className='w-full bg-base-content text-base-300'>Reset</Button>
                     </div>
                 </div>
             </div>

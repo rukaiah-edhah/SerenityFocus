4c9c9708f1070cc65d6cfae27f9974aa50c4c2ad
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2024-05-31 09:02:38 -0400

    Add fetch posts function

---
+++ lib/fetchPosts.ts
@@ -0,0 +1,24 @@
+import sanityClient from "./sanityClient";
+import { Post } from './sanityTypes';
+
+export const getAllPosts = async (): Promise<Post[]> => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    "slug": slug.current,
+    "author": author->{name},
+    "mainImage": {
+      "asset": mainImage.asset->_ref
+    },
+    "categories": categories[]->{title},
+    publishedAt,
+    body
+  }`;
+  try {
+    const posts: Post[] = await sanityClient.fetch(query);
+    return posts;
+  } catch (error) {
+    console.error("Failed to fetch posts", error);
+    return [];
+  }
+};
